b601385a23cdfc89adb20382db6e997325791ef5
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2023-10-20 15:37:42 +0100

    Initial commit

---
+++ src/components/login-screen/LoginScreen.vue
@@ -0,0 +1,9 @@
+<template>
+<d-textfield size="large" label="Sometjhing for yur minds" />
+</template>
+
+<script setup>
+import { DTextfield } from "@deposits/ui-kit-vue";
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from "node:url";
+import path from "node:path";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import bundleScss from "rollup-plugin-bundle-scss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  lib: {
+    entry: path.resolve(__dirname, "src/main.js"),
+    name: "Deposits Vue UI Kit v0.0.0",
+    fileName: (format) => `deposits-vue-ui-kit.${format}.js`,
+  },
+  rollupOptions: {
+    external: ["vue", "vue-router", "@deposits/ui-kit-vue"],
+    output: {
+      // Provide global variables to use in the UMD build
+      // Add external deps here
+      globals: {
+        vue: "Vue",
+        "vue-router": "$router",
+      },
+    },
+  },
+  plugins: [vue(), bundleScss({ exclusive: false })],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
